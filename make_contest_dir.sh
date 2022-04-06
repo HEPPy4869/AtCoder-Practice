a55b866f01a0fae6b69b9ff4282f33f1f4b1c8b8
heppy4869 <yuuki25384869@gmail.com>
2022-04-06 16:03:25 +0900

    Update some notations

--- make_contest_dir.sh
+++ make_contest_dir.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 contest_num=$1
+contest_name="AtCoder Beginner Contest "$1
 date=$( date "+%Y/%m/%d" )
 template=`cat template.cpp`
 
 cd Contests
-mkdir $1
-cd $1
+mkdir "$contest_name"
+cd "$contest_name"
 
-echo "Contest : AtCoder Beginner Contest"$contest_num >> metadata
-echo "URL : https://atcoder.jp/contests/"$contest_num >> metadata
+echo "Contest : "$contest_name >> metadata
+echo "URL : https://atcoder.jp/contests/abc"$contest_num >> metadata
 echo "Author : HEPPy4869" >> metadata
 echo "Created : "$date >> metadata
 
