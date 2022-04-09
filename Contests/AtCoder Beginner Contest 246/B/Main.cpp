3e7b19d5d83995bf6a0c05165a789c073e87d0e7
heppy4869 <yuuki25384869@gmail.com>
2022-04-09 19:29:29 +0900

    Pass A B C

--- Contests/AtCoder Beginner Contest 246/B/Main.cpp
+++ Contests/AtCoder Beginner Contest 246/B/Main.cpp
@@ -3,5 +3,9 @@
 using namespace std;
 
 int main(){
-    
+    int A;
+    int B;
+    cin >> A >> B;
+    double d = sqrt(A*A+B*B);
+    cout << setprecision(12) << A/d << " " << B/d << endl;
 }
--- Contests/AtCoder Beginner Contest 246/C/Main.cpp
+++ Contests/AtCoder Beginner Contest 246/C/Main.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long long ll;
 
 int main(){
-    
+    ll N, K, X;
+    ll ans = 0;
+    ll m = 0;
+    cin >> N >> K >> X;
+    vector<ll> A(N);
+    for(int i=0;i<N;i++) cin >> A[i];
+    for(int i=0;i<N;i++) ans += A[i];
+    for(int i=0;i<N;i++) m += A[i]/X;
+    if(m >= K){
+        ans -= X*K;
+        cout << ans << endl;
+        return 0;
+    }
+    ans -= X*m;
+    for(int i=0;i<N;i++) A[i] %= X;
+    sort(A.begin(), A.end(), greater<ll>());
+    for(int i=0;i<min(N, K-m);i++) ans -= A[i];
+    if(ans>0) cout << ans << endl;
+    else cout << 0 << endl;
 }
