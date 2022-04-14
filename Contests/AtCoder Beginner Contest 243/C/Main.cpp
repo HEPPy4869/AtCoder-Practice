983428ccee77724ba2bb529fee946c7089b5cda1
heppy4869 <yuuki25384869@gmail.com>
2022-04-15 00:01:06 +0900

    Pass C

--- Contests/AtCoder Beginner Contest 243/C/Main.cpp
+++ Contests/AtCoder Beginner Contest 243/C/Main.cpp
@@ -2,6 +2,34 @@
 
 using namespace std;
 
+typedef long long ll;
+
 int main(){
-    
+    ll N;
+    cin >> N;
+    vector<ll> X(N);
+    vector<ll> Y(N);
+    string S;
+    map<ll, vector<ll>> mp;
+
+    for(ll i=0;i<N;i++) cin >> X[i] >> Y[i];
+    cin >> S;
+
+    for(ll i=0;i<N;i++) mp[Y[i]].push_back(i);
+    for(auto p : mp){
+        vector<ll> ids = p.second;
+        vector<pair<ll, char>> ps;
+        for(ll i : ids){
+            ps.emplace_back(X[i], S[i]);
+        }
+        sort(ps.begin(), ps.end());
+        for(ll i=0;i<ps.size()-1;i++){
+            if(ps[i].second == 'R' && ps[i+1].second == 'L'){
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "No" << endl;
+    return 0;
 }
