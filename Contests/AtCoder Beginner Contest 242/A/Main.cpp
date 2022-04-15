acd44d5e1b3f189f5fe3e0ac2be5dbe07a035d66
heppy4869 <yuuki25384869@gmail.com>
2022-04-15 11:45:05 +0900

    Pass A B

--- Contests/AtCoder Beginner Contest 242/A/Main.cpp
+++ Contests/AtCoder Beginner Contest 242/A/Main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    double A, B, C, X;
+    cin >> A >> B >> C >> X;
+    if (A>=X) cout << setprecision(11) << 1.0 << endl;
+    else if(B>=X) cout <<  setprecision(11) << C/(B-A) << endl;
+    else cout << setprecision(11) << 0.0 << endl;
+    return 0;
 }
--- Contests/AtCoder Beginner Contest 242/B/Main.cpp
+++ Contests/AtCoder Beginner Contest 242/B/Main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    string S;
+    cin >> S;
+    sort(S.begin(), S.end());
+    cout << S << endl;
+    return 0;
 }
