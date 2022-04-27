d89504fd9624320c000021218340d0a7b9e13009
heppy4869 <yuuki25384869@gmail.com>
2022-04-27 19:36:35 +0900

    Pass A B

--- Contests/AtCoder Beginner Contest 241/A/Main.cpp
+++ Contests/AtCoder Beginner Contest 241/A/Main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int a[10];
+    int ans;
+    for(int i=0;i<10;i++) cin >> a[i];
+    ans = 0;
+    for(int i=0;i<3;i++) ans = a[ans];
+    cout << ans << endl;
+    return 0;
 }
--- Contests/AtCoder Beginner Contest 241/B/Main.cpp
+++ Contests/AtCoder Beginner Contest 241/B/Main.cpp
@@ -5,5 +5,28 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N, M;
+    cin >> N >> M;
+    vector<int> A(N);
+    vector<int> B(M);
+    vector<int> used(N, 0);
+
+    for(int i=0;i<N;i++) cin >> A[i];
+    for(int i=0;i<M;i++) cin >> B[i];
+    for(int i=0;i<M;i++){
+        int f = 0;
+        for(int j=0;j<N;j++){
+            if(B[i]==A[j] && used[j]==false){
+                used[j]=true;
+                f=1;
+                break;
+            }
+        }
+        if(f==0){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+    return 0;
 }
