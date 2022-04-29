f905dab44c92fd48e4c2a1243090f32cea39555e
heppy4869 <yuuki25384869@gmail.com>
2022-04-29 23:20:52 +0900

    Pass A B

--- Contests/AtCoder Beginner Contest 238/B/Main.cpp
+++ Contests/AtCoder Beginner Contest 238/B/Main.cpp
@@ -5,5 +5,25 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<int> A(N+2);
+    vector<int> X(N+2);
+    for(int i=1;i<=N;i++)cin >> A[i];
+    X[0] = 0;
+    X[N+1] = 360;
+    for(int i=1;i<=N;i++){
+        if(A[i]+X[i-1]<=360) X[i] = X[i-1] + A[i];
+        else {
+            X[i] = X[i-1] + A[i] - 360;
+        }
+    }
+    sort(X.begin(), X.end());
+    int ans = 0;
+    for(int i=0;i<=N;i++){
+        if(X[i+1]-X[i]>ans){
+            ans = X[i+1]-X[i];
+        }
+    }
+    cout << ans << endl;
 }
