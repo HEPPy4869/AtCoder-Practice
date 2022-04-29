1670234ada319a028bca18e538e79d3b0dc75e24
heppy4869 <yuuki25384869@gmail.com>
2022-04-29 17:08:50 +0900

    Add make_run.sh

---
+++ make_run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd Contests
+search_dir="$HOME/kyopro_practice/Contests/*"
+contest_list=()
+for dirpath in $search_dir;
+do
+    cd "$dirpath"
+    echo $dirpath
+    for p in {A..H}
+    do
+        cd $p
+        cat $HOME/kyopro_practice/template.sh > run.sh
+        cd ..
+    done
+    cd ..
+done
