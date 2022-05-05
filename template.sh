a05c27bad6059931013c714053e411b466ce010f
heppy4869 <yuuki25384869@gmail.com>
2022-05-05 18:26:32 +0900

    Update compiler options

--- template.sh
+++ template.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ Main.cpp
+g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -I/opt/ac-library -o ./a.out ./Main.cpp
 ./a.out < input.txt
