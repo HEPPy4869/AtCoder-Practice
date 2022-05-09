ea98e68a12201e1ad3190a3979a8969c0822702a
heppy4869 <yuuki25384869@gmail.com>
2022-05-09 16:46:49 +0900

    Pass 1

--- Practice/Seisen100/1/Main.cpp
+++ Practice/Seisen100/1/Main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int n, x;
+    while(true){
+        cin >> n >> x;
+        if(!n && !x) break;
+        int c=0;
+        for(int i=1;i<=n-2;i++){
+            for(int j=i+1;j<=n-1;j++){
+                for(int k=j+1;k<=n;k++)if(i+j+k==x) c++;
+            }
+        }
+        cout << c << endl;
+    }
+    return 0;
 }
