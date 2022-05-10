b03867323152038d68dd3eaad35311d176f70008
heppy4869 <yuuki25384869@gmail.com>
2022-05-10 21:26:04 +0900

    Pass A

--- Contests/AtCoder Beginner Contest 235/A/Main.cpp
+++ Contests/AtCoder Beginner Contest 235/A/Main.cpp
@@ -5,5 +5,12 @@ using namespace std;
 typedef long long ll;
 
 int main(){
+    string abc;
+    cin >> abc;
+    int bca = stoi(string({abc[1], abc[2], abc[0]}));
+    int cab = stoi(string({abc[2], abc[0], abc[1]}));
     
+    cout << stoi(abc) + bca + cab << endl;
+
+    return 0;
 }
