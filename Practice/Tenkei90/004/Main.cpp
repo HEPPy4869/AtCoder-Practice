07e3e7a4f4e85b2bcab93b7149aba0e5b94c9d1f
heppy4869 <yuuki25384869@gmail.com>
2022-05-16 18:47:33 +0900

    Pass 004

--- Practice/Tenkei90/004/Main.cpp
+++ Practice/Tenkei90/004/Main.cpp
@@ -5,5 +5,24 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int H, W;
+    cin >> H >> W;
+    vector<vector<int>> A(H, vector<int>(W));
+    vector<int> h(H, 0);
+    vector<int> w(W, 0);
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++) cin >> A[i][j];
+    }
+
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++) w[j]+= A[i][j];
+    }
+    for(int i=0;i<W;i++){
+        for(int j=0;j<H;j++) h[j]+= A[j][i];
+    }
+
+    for(int i=0;i<H;i++){
+        for(int j=0;j<W;j++) cout << w[j]+h[i]-A[i][j] << " ";
+        cout << endl;
+    }
 }
