3907bc0f638dac37137e596d321d5cc063c23b94
heppy4869 <yuuki25384869@gmail.com>
2022-05-17 14:16:23 +0900

    Pass 024

--- Practice/Tenkei90/024/Main.cpp
+++ Practice/Tenkei90/024/Main.cpp
@@ -5,5 +5,16 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N, K;
+    cin >> N >> K;
+    vector<int> A(N), B(N);
+
+    for(int i=0;i<N;i++) cin >> A[i];
+    for(int i=0;i<N;i++) cin >> B[i];
+
+    int d=0;
+    for(int i=0;i<N;i++)d+=abs(A[i]-B[i]);
+    if(d>K)cout << "No" << endl;
+    else if((K-d)%2==0) cout << "Yes" << endl;
+    else cout << "No" << endl;
 }
