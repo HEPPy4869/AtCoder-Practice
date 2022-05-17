44c82929b4d7d2183d9d53d9397a46a11f1e35c7
heppy4869 <yuuki25384869@gmail.com>
2022-05-17 15:16:01 +0900

    Pass B C

--- Contests/AtCoder Beginner Contest 235/B/Main.cpp
+++ Contests/AtCoder Beginner Contest 235/B/Main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<int> H(N);
+    for(int i=0;i<N;i++) cin >> H[i];
+
+    int ans=H[0];
+    for(int i=0;i<N-1;i++){
+        if(H[i]>=H[i+1]) break;
+        ans = H[i+1];
+    }
+
+    cout << ans << endl;
+    return 0;
 }
--- Contests/AtCoder Beginner Contest 235/C/Main.cpp
+++ Contests/AtCoder Beginner Contest 235/C/Main.cpp
@@ -5,5 +5,24 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N, Q;
+    cin >> N >> Q;
+    int a;
+    map<int,vector<int>> mp;
+    vector<pair<int,int>> ql(Q);
+
+    for(int i=0;i<N;i++) {
+        cin >> a;
+        mp[a].push_back(i);
+    }
+    for(int i=0;i<Q;i++) cin >> ql[i].first >> ql[i].second;
+    for(int i=0;i<Q;i++) {
+        if(int(mp[ql[i].first].size())>=ql[i].second){
+            cout << mp[ql[i].first][ql[i].second-1]+1 << endl;
+        }
+        else cout << -1 << endl;
+    }
+
+    return 0;
+
 }
