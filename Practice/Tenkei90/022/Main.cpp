ca9aea8a1c06a479c686aaa58a32dadd86ec28b1
heppy4869 <yuuki25384869@gmail.com>
2022-05-17 13:50:41 +0900

    Pass 022

--- Practice/Tenkei90/022/Main.cpp
+++ Practice/Tenkei90/022/Main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    ll A, B, C;
+    cin >> A >> B >> C;
+
+    ll d = gcd(gcd(A, B), C);
+    ll ans = A/d-1 + B/d-1 + C/d-1;
+    cout << ans << endl;
 }
