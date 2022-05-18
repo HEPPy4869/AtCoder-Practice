91e986bccda34b6bc4bf851e16b31b41d4c4dc70
heppy4869 <yuuki25384869@gmail.com>
2022-05-18 15:45:56 +0900

    Pass 033

--- Practice/Tenkei90/033/Main.cpp
+++ Practice/Tenkei90/033/Main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int H, W;
+    cin >> H >> W;
+
+    if(H==1) cout << W << endl;
+    else if(W==1) cout << H << endl;
+    else cout << ((H+1)/2)*((W+1)/2) << endl;
 }
