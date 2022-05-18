43ea58095c69cbd9ab01013149d64201a45b2453
heppy4869 <yuuki25384869@gmail.com>
2022-05-18 15:10:14 +0900

    Pass 027

--- Practice/Tenkei90/027/Main.cpp
+++ Practice/Tenkei90/027/Main.cpp
@@ -5,5 +5,13 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int  N;
+    cin >> N;
+    vector<string> S(N);
+    map<string, int> mp;
+    for(int i=0;i<N;i++) cin >> S[i];
+    for(int i=0;i<N;i++){
+        if(mp[S[i]]==NULL){ mp[S[i]]=true; cout << i+1 << endl;}
+    }
+    return 0;
 }
