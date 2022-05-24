32731b6a4ce700e043ceb31fb14477d51822418f
heppy4869 <yuuki25384869@gmail.com>
2022-05-24 16:13:39 +0900

    Pass 061

--- Practice/Tenkei90/061/Main.cpp
+++ Practice/Tenkei90/061/Main.cpp
@@ -4,6 +4,33 @@ using namespace std;
 
 typedef long long ll;
 
+void solve_with_vector(int Q){
+    vector<int> a;
+    for(int i=0;i<Q;i++){
+        int t, x;
+        cin >> t >> x;
+        if(t==1) a.insert(a.begin(), x);
+        else if(t==2) a.push_back(x);
+        else cout << a[x-1] << endl;
+    }
+    return;
+}
+
+void solve_with_deque(int Q){
+    deque<int> dq;
+    for(int i=0;i<Q;i++){
+        int t, x;
+        cin >> t >> x;
+        if(t==1) dq.push_front(x);
+        else if(t==2) dq.push_back(x);
+        else cout << dq[x-1] << endl;
+    }
+}
+
 int main(){
-    
+    int Q;
+    cin >> Q;
+    solve_with_deque(Q);
+    //solve_with_vector(Q);
+    return 0;
 }
