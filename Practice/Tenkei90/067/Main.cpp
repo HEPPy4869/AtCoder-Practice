cf8b477e3c7914d2b1efd4311ec810c6022d5f05
heppy4869 <yuuki25384869@gmail.com>
2022-05-25 23:51:38 +0900

    Pass 067

--- Practice/Tenkei90/067/Main.cpp
+++ Practice/Tenkei90/067/Main.cpp
@@ -5,5 +5,20 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    string N;
+    int K;
+    cin>>N>>K;
+    for(int i=0;i<K;i++){
+        ll A=0;
+        for(char c:N)A=A*8+c-'0';
+        N="";
+        for(;A;A/=9)N+=(char)(A%9+'0');
+        reverse(N.begin(),N.end());
+        for(char&c:N){
+            if(c=='8')c='5';
+        }
+        if(N=="")N="0";
+    }
+    cout<<N<<endl;
+
 }
