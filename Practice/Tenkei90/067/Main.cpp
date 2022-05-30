fa0961f20e9f286e831c86b2afd343b08583a341
heppy4869 <yuuki25384869@gmail.com>
2022-05-30 17:24:21 +0900

    Fix 067

--- Practice/Tenkei90/067/Main.cpp
+++ Practice/Tenkei90/067/Main.cpp
@@ -4,21 +4,36 @@ using namespace std;
 
 typedef long long ll;
 
+ll base8_to_ll(string N){
+    ll ans=0, u=1;
+    for(int i=int(N.size())-1;i>=0;i--){
+        ans+=1LL*(N[i]-'0')*u;
+        u*=8;
+    }
+    return ans;
+}
+
+string ll_to_base9(ll N){
+    if(N==0) return "0";
+    string ans;
+    while(N>0){
+        char c=((N%9)+'0');
+        ans=c+ans;
+        N/=9;
+    }
+    return ans;
+}
+
 int main(){
     string N;
     int K;
-    cin>>N>>K;
+    cin >> N >> K;
     for(int i=0;i<K;i++){
-        ll A=0;
-        for(char c:N)A=A*8+c-'0';
-        N="";
-        for(;A;A/=9)N+=(char)(A%9+'0');
-        reverse(N.begin(),N.end());
-        for(char&c:N){
-            if(c=='8')c='5';
+        N=ll_to_base9(base8_to_ll(N));
+        for(int i=0;i<int(N.size());i++){
+            if(N[i]=='8') N[i]='5';
         }
-        if(N=="")N="0";
     }
-    cout<<N<<endl;
-
+    cout << N << endl;
+    return 0;
 }
