e6907300fcd409605c08ce3765b4307a7568ed27
heppy4869 <yuuki25384869@gmail.com>
2022-05-30 18:24:01 +0900

    Pass 002

--- Practice/Tenkei90/002/Main.cpp
+++ Practice/Tenkei90/002/Main.cpp
@@ -5,5 +5,23 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    for(int bit=0;bit<(1<<N);bit++){
+        int c=0;
+        for(int i=0;i<N;i++){
+            if((bit>>i)&1) c++;
+            else c--;
+            if(c<0) break;
+        }
+        if(!c){
+            string ans;
+            for(int i=N-1;i>=0;i--){
+                if((bit>>i)&1) ans+=')';
+                else ans+='(';
+            }
+            cout << ans << endl;
+        }
+    }
+    return 0;
 }
