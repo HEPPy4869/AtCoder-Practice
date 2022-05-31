6fc2226c34742a0ea5ac16a576eddcefde8b9b92
heppy4869 <yuuki25384869@gmail.com>
2022-05-31 14:52:17 +0900

    Pass 014

--- Practice/Tenkei90/014/Main.cpp
+++ Practice/Tenkei90/014/Main.cpp
@@ -5,5 +5,17 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    ll N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    for(int i=0;i<N;i++)cin >> A[i];
+    for(int i=0;i<N;i++)cin >> B[i];
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    ll sum=0;
+    for(int i=0;i<N;i++){
+        sum+=abs(A[i]-B[i]);
+    }
+    cout << sum << endl;
+    return 0;
 }
