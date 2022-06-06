a5721d2558bb8a4b55e05d1e016dbbde0cae6395
heppy4869 <yuuki25384869@gmail.com>
2022-06-06 15:44:31 +0900

    Pass 018

--- Practice/Tenkei90/018/Main.cpp
+++ Practice/Tenkei90/018/Main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 typedef long long ll;
 
+#define PI 3.14159265359
+
 int main(){
-    
+    double T,L,Q;
+    double X,Y;
+    cin >> T >> L >> X >> Y >> Q;
+    double r=L/2.0;
+    for(int i=0;i<Q;i++) {
+        double E;
+        cin >> E;
+        double theta=360.0*(E/T);
+        double py=-1*r*sin(PI*theta/180.0),pz=r-(1*r*cos(PI*theta/180.0));
+        double a=sqrt(X*X+(py-Y)*(py-Y));
+        cout << setprecision(12) << atan2(pz,a)*180/PI << endl;
+    }
+
+    return 0;
+
 }
