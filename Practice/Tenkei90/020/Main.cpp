bc6416554aa8e3b94d30febb887ceb2575477680
heppy4869 <yuuki25384869@gmail.com>
2022-06-07 19:00:20 +0900

    Pass 020

--- Practice/Tenkei90/020/Main.cpp
+++ Practice/Tenkei90/020/Main.cpp
@@ -5,5 +5,14 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    ll A, B, C;
+    cin >> A >> B >> C;
+
+    ll d=1;
+    for(ll i=0;i<B;i++) d*=C;
+
+    if(A<d) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
 }
