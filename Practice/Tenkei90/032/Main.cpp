5e60c55472335d3f79f9a0d5b3b099f6a2712a4b
heppy4869 <yuuki25384869@gmail.com>
2022-06-07 19:00:33 +0900

    Pass 032

--- Practice/Tenkei90/032/Main.cpp
+++ Practice/Tenkei90/032/Main.cpp
@@ -5,5 +5,34 @@ using namespace std;
 typedef long long ll;
 
 int main(){
+    int N,M;
+    cin >> N;
+    vector<vector<int>> A(N,vector<int> (N));
+    for(int i=0;i<N;i++) for(int j=0;j<N;j++) cin >> A[i][j];
+    cin >> M;
+    vector<vector<int>> b(N,vector<int> (N,0));
+    for(int i=0;i<M;i++) {
+        int x,y;
+        cin >> x >> y;
+        b[x-1][y-1]=1;
+        b[y-1][x-1]=1;
+    }
+
+    vector<int> p(N);
+    for(int i=0;i<N;i++) p[i]=i; //i区間を何番目の選手が走るか
+    int ans=(1<<30);
+    do{
+        int sum=0; int flag=0;
+        for(int i=0;i<N;i++){
+            sum+=A[p[i]][i];
+            if(i>0&&b[p[i-1]][p[i]]) flag=1;
+        }
+        if(!flag) ans=min(ans,sum);
+
+    }while(next_permutation(p.begin(),p.end()));
+
+    if(ans!=(1<<30))cout << ans << endl;
+    else cout << -1 << endl;
     
+    return 0;
 }
