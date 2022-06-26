83d563c8c1bad6af88840ae509f84c24969b716d
heppy4869 <yuuki25384869@gmail.com>
2022-06-27 08:07:43 +0900

    Pass 038

--- Practice/Tenkei90/038/Main.cpp
+++ Practice/Tenkei90/038/Main.cpp
@@ -5,5 +5,12 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    ll A, B;
+    cin >> A >> B;
+
+    ll l = B/gcd(A,B);
+    if(l>1000000000000000000LL/A) cout << "Large" << endl;
+    else cout << l*A << endl;
+
+    return 0;
 }
