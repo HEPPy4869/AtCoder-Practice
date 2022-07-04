4c64acd10375099836794ab0278179439827edc7
heppy4869 <yuuki25384869@gmail.com>
2022-07-05 08:12:49 +0900

    Pass 046

--- Practice/Tenkei90/046/Main.cpp
+++ Practice/Tenkei90/046/Main.cpp
@@ -5,5 +5,37 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<int> A(46,0), B(46,0), C(46,0);
+
+    for(int i=0;i<N;i++){
+        int x;cin >> x;
+        x%=46;
+        A[x]++;
+    }
+    for(int i=0;i<N;i++){
+        int x;cin >> x;
+        x%=46;
+        B[x]++;
+    }
+    for(int i=0;i<N;i++){
+        int x;cin >> x;
+        x%=46;
+        C[x]++;
+    }
+
+    ll ans=0;
+    for(int i=0;i<46;i++){
+        for(int j=0;j<46;j++){
+            for(int k=0;k<46;k++){
+                if((i+j+k)%46==0) ans+=1LL*A[i]*B[j]*C[k];
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+
 }
