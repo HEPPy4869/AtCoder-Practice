5b45e9872f5afb4b8b2ba8c45636209dc2a35ff1
heppy4869 <yuuki25384869@gmail.com>
2022-07-05 08:13:15 +0900

    Pass 048

--- Practice/Tenkei90/048/Main.cpp
+++ Practice/Tenkei90/048/Main.cpp
@@ -5,5 +5,30 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N,K;
+    cin >> N >> K;
+    vector<int> C;
+
+    for(int i=0;i<N;i++){
+        int a, b;
+        cin >> a >> b;
+        C.push_back(b);
+        C.push_back(a-b);
+    }
+
+    sort(C.begin(),C.end(),greater<int>());
+    ll ans=0;
+    for(auto x:C){
+        ans+=x;
+        K--;
+        if(K<=0){
+            cout << ans << endl;
+            return 0;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+
 }
