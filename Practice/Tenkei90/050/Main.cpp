114b9232d536678532b9cb5a3d2f43aa24160c6e
heppy4869 <yuuki25384869@gmail.com>
2022-07-06 07:52:42 +0900

    Pass 050

--- Practice/Tenkei90/050/Main.cpp
+++ Practice/Tenkei90/050/Main.cpp
@@ -5,5 +5,18 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N, L;
+    cin >> N >> L;
+    vector<ll> dp(N+1,0);
+
+    dp[0]=1;
+    for(int i=1;i<N+1;i++){
+        dp[i]+=dp[i-1];
+        if(i-L>=0) dp[i]+=dp[i-L];
+        dp[i]%=1000000007;
+    }
+
+    cout << dp[N] << endl;
+
+    return 0;
 }
