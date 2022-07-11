7da03e94313af5365c5a16ed81550a5777796ec7
heppy4869 <yuuki25384869@gmail.com>
2022-07-11 09:11:21 +0900

    Pass 052

--- Practice/Tenkei90/052/Main.cpp
+++ Practice/Tenkei90/052/Main.cpp
@@ -5,5 +5,22 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    int N;
+    cin >> N;
+    vector<vector<int>> A(N,vector<int>(6));
+    for(int i=0;i<N;i++){
+        for(int j=0;j<6;j++) cin >> A[i][j];
+    }
+
+    ll ans=1;
+    for(int i=0;i<N;i++){
+        ll d=0;
+        for(int j=0;j<6;j++)d+=A[i][j];
+        ans*=d;
+        ans%=1000000007LL;
+    }
+
+    cout << ans << endl;
+
+    return 0;
 }
