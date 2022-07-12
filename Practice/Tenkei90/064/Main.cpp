85956c4d071976930111612b01e56dc32797547a
heppy4869 <yuuki25384869@gmail.com>
2022-07-12 16:35:51 +0900

    Pass 064

--- Practice/Tenkei90/064/Main.cpp
+++ Practice/Tenkei90/064/Main.cpp
@@ -5,5 +5,25 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-    
+    ll N,Q,ans=0;
+    cin >> N >> Q;
+    vector<ll> A(N),B(N);
+    for(int i=0;i<N;i++) cin >> A[i];
+
+    for(int i=0;i<N-1;i++){
+        B[i]=A[i+1]-A[i];
+        ans+=abs(B[i]);
+    }
+
+    for(int i=0;i<Q;i++){
+        int L,R,V; cin >> L >> R >> V; L--;R--;
+        ll f=abs(B[L-1])+abs(B[R]);
+        if(L>=1)B[L-1]+=V;
+        if(R<N-1)B[R]-=V;
+        ll b=abs(B[L-1])+abs(B[R]);
+        ans+=(b-f);
+        cout << ans << endl;
+    }
+
+    return 0;
 }
